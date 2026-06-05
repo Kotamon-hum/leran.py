edd9aa5ba9f7e4b4b19023251b14d4d92624a27d
humhum <fumakotajp@gmail.com>
2026-06-05 21:23:32 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,35 @@
 #print構文(文字を出力できる)
 print("Hello World!")
 
-#input構文(ユーザーの入力を受け取る。又は競技プログラミングでは入力を*文字列*で受け取る)
+#input構文(ユーザーの入力を受け取る。又は競技プログラミングでは入力を**文字列**で受け取る)
 name = input("あなたの名前は？")
+
 num = int(input("数字を入力してください"))
+
 int = int(input().split)
+
+int = list(map(int, input().split()))
+
+#for構文(指定した回数下に付属する行為を繰り返す)
+for i in range(9):
+ print ("piyo")
+  
+num = int(input("感謝の数字をいれてください"))
+for i in range(num):
+ print("ty")
+
+#if構文(ifで条件分岐する)
+while True:
+ num = int(input())
+ if num == 0:
+  print("これはゼロです")
+  break
+ elif num == 1:
+  print("これはイチです")
+  break
+ elif num == 2:
+  print("これはニです")
+  break
+ else:
+  print("もう一度入力してください")
+  continue
